26399b253808f8dd0465d29ca24372ef0d38b106
ARyback <adam.ryback@gmail.com>
2022-05-20 19:59:35 -0500

    Practiced with 2 different useStates and replacing them now

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import React, { useState, useEffect } from 'react';
 function App() {
 
   const [resourceType, setResourceType] = useState('posts');
+  const [items, setItems] = useState([]);
 
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
       .then(response => response.json())
-      .then(json => console.log(json))
+      .then(json => setItems(json))
   }, [resourceType])
 
   return (
@@ -18,6 +19,9 @@ function App() {
         <button onClick={() => setResourceType('comments')}>Comments</button>
       </div>
       <h1>{resourceType}</h1>
+      {items.map(item => {
+        return <pre>{JSON.stringify(item)}</pre>
+      })}
     </>
   );
 }
